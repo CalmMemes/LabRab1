c1da7b5179b96a581207afba91f0200cb56df87d
admin <admin@LAPTOP-7KRDQTII>
2020-09-05 13:47:32 +0300

    labrab1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 35, f = 5, x = 4, y = 3;
+            int Z;
+            Z = a/f + y * f - (f+y)/x;
+
+            Console.WriteLine("Лабораторная работа №1. Разработка консольного приложения");
+            Console.WriteLine("Сухарев Роман Олегович");
+            Console.WriteLine("ИНС-192");
+            Console.WriteLine("04.09.01");
+            Console.WriteLine("г.Ставрополь ул.50 лет ВЛКСМ дом 81/1");
+            Console.WriteLine("Информатика");
+            Console.WriteLine("Делать игры :)");
+
+            Console.WriteLine("Решим уравнение №6");
+            Console.WriteLine("Z = a/f + y * f - (f+y)/x");
+            Console.WriteLine("Перменная a = {0}", a);
+            Console.WriteLine("Переменная x = {0}", x);
+            Console.WriteLine("Переменная y = {0}", y);
+            Console.WriteLine("Переменная f = {0}", f);
+            Console.WriteLine("Ответ Z = {0}", Z);
+
+            Console.ReadKey();
+        }
+    }
+}
